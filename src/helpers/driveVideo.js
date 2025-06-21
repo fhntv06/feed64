cac5cc2e04b62f172ec7f70d5685c3dcd0bccee3
kartem <k6art5@gmail.com>
2025-06-21 12:55:26 +0700

    clear branch

--- src/helpers/driveVideo.js
+++
@@ -1,4 +0,0 @@
-export const driveVideo = (state, id) => {
-    const video = document.querySelector(`${id} video`);
-    state ? video.pause() : video.play();
-};
--- src/hooks/intersectionObserver.js
+++
@@ -1,9 +0,0 @@
-import { useInView } from 'react-intersection-observer';
-
-const IntersectionObserver = () => {
-  return { ref, inView } = useInView({
-    threshold: 1,
-  });
-};
-
-export default IntersectionObserver;
